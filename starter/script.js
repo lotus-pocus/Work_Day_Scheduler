aeb2055ada3c43390e3a694ff03e9e5dc18eef1c
lotus-pocus <116104241+lotus-pocus@users.noreply.github.com>
2022-12-13 18:00:42 +0000

    added current day and date, added time-block textboxes, added save button

--- starter/script.js
+++ starter/script.js
@@ -1,10 +1,45 @@
-var startOfBusiness = moment('09', 'HH');
+var time = moment().format('dddd, MMM Do YYYY');
+var startOfBusiness = moment ('09', 'HH');
 
-var hourRow = $('#timeRow');
+// set the current day at the top of the calendar
+$("#currentDay").append(time);
 
-while (startOfBusiness.hour()<18) {
-    startOfBusiness.add(1, 'hours');
-    console.log (startOfBusiness.format('ddd Do hh:MMa'));
+//create variables for different time-blocks
+var firstTime = $('#timeRow1');
+var secondTime = $('#timeRow2');
+var thirdTime = $('#timeRow3');
+var fourthTime = $('#timeRow4');
+var fifthTime = $('#timeRow5');
+var sixthTime = $('#timeRow6');
+var seventhTime = $('#timeRow7');
+var eigthTime = $('#timeRow8');
+var ninthTime = $('#timeRow9');
+
+// $('#timeRow1') = moment(09, 'HH');
+
+function savelogEntry(arr){
+    localStorage.setItem('firstTime', JSON.stringify(arr));
 }
 
-hourRow.innerText = startOfBusiness.format('ddd Do hh:MMa');
+//creat event listeners to save items
+function addfirstTime() {
+var keypressed = event.keycode;
+}
+
+
+function initial() {
+    firstTime.addEventListener('click', addfirstTime);
+    var logEntry = firstTime.value;
+    firstTime.push(logEntry);
+    saveFirstTime(firstTime);
+}
+
+initial();
+
+
+// while (startOfBusiness.hour() < 18) {
+//     console.log (startOfBusiness.format('ddd Do hh:MM a'));
+//     startOfBusiness.add(1,'hours');
+//     var newRow = $("<div>").addClass("row").attr("id");
+// }
+
